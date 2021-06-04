ce100eba34cc7912f8e8247966bd77b7dd3e3b2c
chanyub Park <mrchypark@gmail.com>
2021-06-04 18:35:12 +0900

    update http to https

--- tests/testthat/test-get_speech.R
+++ tests/testthat/test-get_speech.R
@@ -1,6 +1,6 @@
 context("test-get_speech.R")
 
 test_that("get speech text data", {
-  dat <- get_speech("http://www.pa.go.kr/research/contents/speech/index.jsp?spMode=view&catid=c_pa02062&artid=1311235")
+  dat <- get_speech("https://www.pa.go.kr/research/contents/speech/index.jsp?spMode=view&catid=c_pa02062&artid=1311235")
   expect_equal(dat$date, "1957.07.04")
 })
